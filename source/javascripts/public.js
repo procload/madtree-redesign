f7c69d18c019e3dc856c203664265550c9eb8f37
procload <ryan@ryanmerrill.net>
2016-10-11 20:19:23 -0400

    More work on responsive navigation

--- source/javascripts/public.js
+++ source/javascripts/public.js
@@ -1,8 +1,45 @@
 $(document).ready(function() {
 
+    // Slideshow Javascript
     $('.slideshow').slick({
         dots: true
     });
 
     $('.on-tap-slideshow').slick();
+
+    // Navigation Javascript
+    $('.nav-toggle').on('click', function(e) {
+        e.preventDefault();
+
+        $mainNav = $('.site-nav.main');
+        if ($mainNav.hasClass('open')) {
+            $mainNav.removeClass('open');
+        } else {
+            $mainNav.addClass('open');
+        }
+    })
+
+    // Clone the subnav and append it to the active list element
+    // so responsive navigation works
+    activeGroup = $('.site-nav.main .nav-list .active').data("children");
+    $subNav = $('.subnav');
+    if ($subNav.data('group') == activeGroup) {
+        copiedNav = $subNav.clone().addClass('cloned')
+        $('.site-nav.main .active').parent('li').append(copiedNav);
+    }
+
+    $('.active i').on('click', function(e) {
+        e.preventDefault();
+
+        $subNav = $('.subnav.cloned');
+        if ($subNav.hasClass('open')) {
+            $subNav.removeClass('open');
+            $(this).removeClass('fa-caret-down').addClass('fa-caret-right');
+        } else {
+            $subNav.addClass('open');
+            $(this).removeClass('fa-caret-right').addClass('fa-caret-down');
+        }
+    })
+
+
 });
